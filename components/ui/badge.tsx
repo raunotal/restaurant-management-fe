1f41794ee7ee295c2bae44f94f74e374fe1c35c8
rauno-tali <93924339+rauno-tali@users.noreply.github.com>
2025-02-07 20:15:13 +0200

    finish recipes grid

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -6,15 +6,17 @@ type BadgeProps = {
   color?: "primary" | "active";
   dot?: boolean;
   text: string;
+  className?: string;
   onClick?: () => void;
 };
 
-export default function Badge({ text, color, dot, onClick }: BadgeProps) {
+export default function Badge({ text, color, dot, className, onClick }: BadgeProps) {
   const badgeClassName = classNames(
-    "bg-gray-200 text-gray-600 rounded-full px-2.5 py-2 flex items-center gap-2 text-[12px] leading-[1] font-semibold",
+    "bg-gray-200 text-gray-600 rounded-full px-2.5 py-2 flex items-center gap-2 text-[12px] leading-[1] font-semibold w-min",
     color === "active" && "bg-green-100 text-green-600",
     color === "primary" && "bg-indigo-100 text-indigo-600",
-    !!onClick && "cursor-pointer"
+    !!onClick && "cursor-pointer",
+    className
   );
 
   return (
