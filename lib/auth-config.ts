d049d546f4675702289c2c34f453457cfc0798dd
rauno-tali <93924339+rauno-tali@users.noreply.github.com>
2025-02-14 01:25:56 +0200

    add trustHost config to auth js for running in docker environment

--- lib/auth-config.ts
+++ lib/auth-config.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import MicrosoftEntraID from "next-auth/providers/microsoft-entra-id";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: process.env.AUTH_TRUST_HOST === "true",
   secret: process.env.AUTH_SECRET,
   providers: [
     MicrosoftEntraID({
